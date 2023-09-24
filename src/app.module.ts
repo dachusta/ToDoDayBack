3df5d10aee2f89a8874cf318c8261cfa309ccaac
ChustaD <chusta2013@mail.ru>
2023-09-24 11:16:14 +0300

    Added the @nests/config package to use dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DaysModule } from './days/days.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [DaysModule, TasksModule],
+  imports: [
+    DaysModule,
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
