15521e0de556c892dbc4480c5427d1176b064b88
ChustaD <chusta2013@mail.ru>
2023-09-26 16:31:57 +0300

    Added endpoints

--- src/days/days.controller.ts
+++ src/days/days.controller.ts
@@ -1,38 +1,53 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Res } from '@nestjs/common';
 import { DaysService } from './days.service';
+import * as admin from 'firebase-admin';
 
 const chatId = '';
-
-let intervalId = null;
-
-let days = [];
-let today = null;
-let modifyDate = null;
+const intervals = {};
 
 @Controller('days')
 export class DaysController {
   constructor(private readonly daysService: DaysService) {}
 
   @Get('getList')
-  getList() {
-    return days;
+  async getList(@Req() req) {
+    console.log(req.query.userId);
+
+    try {
+      const dbRef = admin.database().ref();
+      const snapshot = await dbRef.child(`${req.query.userId}/days`).get();
+
+      if (snapshot.exists()) {
+        const days = snapshot.val();
+
+        console.log(days);
+        return days;
+      } else {
+        console.log('No data available');
+        return [];
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
   }
 
   @Post('setList')
-  setList(req, res) {
-    res.json({
-      success: true,
-    });
-    clearInterval(intervalId);
+  setList(@Body() days, @Req() req, @Res() res) {
+    console.log(1);
+    admin.database().ref(`${req.query.userId}/days`).set(days);
 
-    days = req.body;
-    today = days[0];
+    let lastMovedDate = null;
+    let today = days[0];
 
-    intervalId = setInterval(() => {
+    clearInterval(intervals[req.query.userId]);
+
+    intervals[req.query.userId] = setInterval(() => {
       const currentDate = new Date().getDate();
       const currentHours = new Date().getHours();
       const currentMinutes = new Date().getMinutes();
 
+      // Уведомление о запланированной задаче
       for (const task of today.tasks) {
         if (
           currentHours === getHours(task.time) &&
@@ -44,17 +59,20 @@ export class DaysController {
         }
       }
 
+      // Перенос прошедшего дня в конец
       if (
-        currentDate !== modifyDate &&
+        currentDate !== lastMovedDate &&
         currentHours === 23 &&
         currentMinutes === 59
       ) {
-        modifyDate = currentDate;
+        lastMovedDate = currentDate;
 
         const pastDay = days.shift();
         days.push(pastDay);
 
         today = days[0];
+
+        admin.database().ref(`${req.query.userId}/days`).set(days);
       }
     }, 59999);
 
@@ -66,16 +84,83 @@ export class DaysController {
       if (!time) return null;
       return parseInt(time.split(':')[1]);
     }
+    console.log(2);
+
+    return days;
   }
 
   @Post('setTimeTask')
-  setTimeTask() {
+  async setTimeTask(@Body() { dayId, taskId, time }, @Req() req) {
+    let days = null;
+
+    // 1. GET DAYS
+    try {
+      const dbRef = admin.database().ref();
+      const snapshot = await dbRef.child(`${req.query.userId}/days`).get();
+
+      if (snapshot.exists()) {
+        days = snapshot.val();
+
+        // 2. SET TIME TO TASK
+        days.forEach((day) => {
+          if (dayId === day._id) {
+            day.tasks.forEach((task) => {
+              if (task._id === taskId) {
+                task.time = time;
+              }
+            });
+          }
+        });
+        // -------
+
+        // 3. SET DAYS
+        admin.database().ref(`${req.query.userId}/days`).set(days);
+      } else {
+        console.log('No data available');
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
+    // --------
+
     return 'setTimeTask';
   }
-  // days/setTimeTask (dayID, taskId) - устанавливать время только в режиме просмотра (в режиме редактора элемент readonly/disabled)
+
   @Post('setDoneTask')
-  setDoneTask() {
+  async setDoneTask(@Body() { dayId, taskId, checked }, @Req() req) {
+    let days = null;
+
+    // 1. GET DAYS
+    try {
+      const dbRef = admin.database().ref();
+      const snapshot = await dbRef.child(`${req.query.userId}/days`).get();
+
+      if (snapshot.exists()) {
+        days = snapshot.val();
+
+        // 2. SET CHECKED TO TASK
+        days.forEach((day) => {
+          if (dayId === day._id) {
+            day.tasks.forEach((task) => {
+              if (task._id === taskId) {
+                task.checked = checked;
+              }
+            });
+          }
+        });
+        // -------
+
+        // 3. SET DAYS
+        admin.database().ref(`${req.query.userId}/days`).set(days);
+      } else {
+        console.log('No data available');
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
+    // --------
     return 'setDoneTask';
   }
-  // days/setDoneTask (dayID, taskId) - устанавливать в выполнено только в режиме просмотра
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,26 +1,51 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-
-let tasks = [];
+import * as admin from 'firebase-admin';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get('getList')
-  getList(req, res) {
-    res.json({
-      success: true,
-      data: tasks,
-    });
+  async getList(@Req() req) {
+    // ========================
+    // const dataSnapshot = await admin.database().ref(`${req.query.userId}/tasks`).get();
+    // const tasks = dataSnapshot.val()
+    // ========================
+    console.log(req.query.userId);
+
+    try {
+      const dbRef = admin.database().ref();
+      const snapshot = await dbRef.child(`${req.query.userId}/tasks`).get();
+
+      if (snapshot.exists()) {
+        const tasks = snapshot.val();
+
+        console.log(tasks);
+        return tasks;
+      } else {
+        console.log('No data available');
+        return [];
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
   }
 
   @Post('setList')
-  setList(req, res) {
-    res.json({
-      success: true,
-    });
-
-    tasks = req.body;
+  setList(@Body() tasks, @Req() req, @Res() res) {
+    admin.database().ref(`${req.query.userId}/tasks`).set(tasks);
+    console.log(tasks);
+    // res.status(HttpStatus.CREATED).send();
+    return tasks;
   }
 }
