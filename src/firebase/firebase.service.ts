e0f72930a47021c3d4db584ffacfe11cf3a1b207
ChustaD <chusta2013@mail.ru>
2023-10-11 23:25:27 +0300

    Added setUser and getUsers

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -28,4 +28,52 @@ export class FirebaseService {
   async write(userId: string, path: string, data) {
     return await admin.database().ref(`${userId}/${path}`).set(data);
   }
+
+  async getUsers() {
+    try {
+      const dataSnapshot = await admin.database().ref('users').get();
+
+      if (dataSnapshot.exists()) {
+        const data = dataSnapshot.val();
+
+        console.log(data);
+        return data;
+      } else {
+        console.log('No data available');
+        return [];
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
+  }
+  async setUser(userId: string) {
+    let users = [];
+
+    try {
+      const dataSnapshot = await admin.database().ref('users').get();
+
+      if (dataSnapshot.exists()) {
+        const data = dataSnapshot.val();
+
+        console.log(data);
+        users = data;
+      } else {
+        console.log('No data available');
+        users = [];
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
+
+    if (!users.includes(userId)) {
+      users.push(userId);
+      return await admin.database().ref('users').set(users);
+    } else {
+      return 'Already exists';
+    }
+  }
+
+  // async deleteUser(userId: string) {}
 }
